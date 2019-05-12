ce752a372ede4754000920bd811c805dd80107c6
cokeend <coke09@live.com>
2019-05-12 17:38:22 +0700

    question  layout

--- app/src/main/java/th/ac/buu/se/pers/QuestionListActivity.kt
+++ app/src/main/java/th/ac/buu/se/pers/QuestionListActivity.kt
@@ -2,11 +2,16 @@ package th.ac.buu.se.pers
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.widget.LinearLayoutManager
+import android.support.v7.widget.RecyclerView
+import kotlinx.android.synthetic.main.activity_question_list.*
 
 class QuestionListActivity : AppCompatActivity() {
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_question_list)
+
     }
 }
