5f945ab4e5dce0c1d15d74de57199689d439220d
kiratae <kirataetwo@gmail.com>
2019-05-12 14:48:58 +0700

    init files

---
+++ app/src/main/java/th/ac/buu/se/pers/ExamInsertActivity.kt
@@ -0,0 +1,12 @@
+package th.ac.buu.se.pers
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class ExamInsertActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_exam_insert)
+    }
+}
---
+++ app/src/main/java/th/ac/buu/se/pers/QuestionInsertActivity.kt
@@ -0,0 +1,12 @@
+package th.ac.buu.se.pers
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class QuestionInsertActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_question_insert)
+    }
+}
